0c3b6fe7d2cbee5b8c78e4dcc989ba03b63ce552
pjbahaduri7 <58697113+pjbahaduri7@users.noreply.github.com>
2020-01-23 11:47:07 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,5 +1,5 @@
 #Python Program to find whether a number is prime or not
-num = ('Enter the number: ')
+num = int(input('Enter the number: '))
 for i in range(2, num):
     if num % i == 0:
         print('It is not a prime number')
